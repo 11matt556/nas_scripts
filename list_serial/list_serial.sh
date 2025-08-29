c713c2b5180340ea40fdbf61a31f4eac9eb23cea
Matthew Howell <llmatt556@hotmail.com>
2025-08-29 13:03:32 -0400

    Use column for formatting. Added HCTL bus and RAID info

--- list_serial/list_serial.sh
+++ list_serial/list_serial.sh
@@ -1,17 +1,46 @@
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+results=""
 for drive in `lsblk --nodeps -n -o name`; do
   #sas=$(/usr/sbin/smartctl -a /dev/$drive | grep --count "SAS")
   issas="no"
 
+  hctl="$(lsblk --nodeps -no hctl /dev/$drive)"
+
   if [ $(/usr/sbin/smartctl -a /dev/$drive | grep --count "SAS") = 1 ]; then
     tmp=$(/usr/sbin/smartctl -i /dev/$drive | grep Serial)
-    result=${tmp/#"Serial number:        "}
-    issas="yes"
+    serial=${tmp/#"Serial number:        "}
+    issas="${GREEN}yes${ENDCOLOR}"
   else
-    result="$(lsblk --nodeps -no serial /dev/$drive)"
-    issas="no "
+    serial="$(lsblk --nodeps -no serial /dev/$drive)"
+    issas="${RED}no${ENDCOLOR}"
   fi
 
   size=$(lsblk --nodeps -no size /dev/$drive)
+  
+  raid=$(/usr/sbin/mdadm -Q /dev/$drive | tail -1)
+  
+  if [ $(/usr/sbin/mdadm -Q /dev/$drive | tail -1 | grep --count "device active") = 1 ]; then
+    raid="active"
+  elif [ $(/usr/sbin/mdadm -Q /dev/$drive | tail -1 | grep --count "device mismatch") = 1 ]; then
+    raid="failure"
+  else
+    raid="inactive"
+  fi
+  
 
-  echo "/dev/$drive SAS: $issas Serial: $result Size: $size"
+  #echo "/dev/$drive SAS: $issas Serial: $serial Size: $size hctl: $hctl"
+  results+="/dev/$drive $issas $size $hctl $raid $serial\n"
 done
+
+print_var=""
+#for result in ${results[@]}; do
+  #echo $result
+  #printvar+=$($result)
+#done
+
+echo -e $results
+
+(echo -e "DRIVE SAS SIZE HCTL RAID SERIAL\n"; echo -e $results) | column -t
